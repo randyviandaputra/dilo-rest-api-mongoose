c03d6404052dcd12497450c9b1e64c2fe8a0466b
Randy Vianda Putra <modulloe@gmail.com>
2019-12-06 15:46:49 +0700

    feat: cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
@@ -7,6 +8,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors());
 app.use(userRouter)
 app.use(taskRouter)
 
