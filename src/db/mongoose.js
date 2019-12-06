6456450d6633cbcecd3c5880fc740466cc6b8c4c
Randy Vianda Putra <modulloe@gmail.com>
2019-12-06 12:51:59 +0700

    fix: spliting code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,7 @@
-
-// init mongoose
 const mongoose = require('mongoose')
 
-// connection
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-  useNewUrlParser: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 })
