fc6826c8f646081ed2b140a4a3a4c2411a100f87
Randy Vianda Putra <modulloe@gmail.com>
2019-12-07 09:17:00 +0700

    feat: login, logout, upload

--- src/models/task.js
+++ src/models/task.js
@@ -1,42 +1,27 @@
-
 const mongoose = require('mongoose')
 
-<<<<<<< HEAD
-const taskSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  }, 
-  completed: {
-    type: Boolean,
-    default: false
+const taskSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-})
+)
 
 const Task = mongoose.model('Task', taskSchema)
 
-
 module.exports = Task
-=======
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-})
-
-module.exports = Task
->>>>>>> 1d42c967cb1987d6d4b38f0a1454c8be4224b098
