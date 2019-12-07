5c9f1c85452a470a38d8d7710e1dbadb0daf4e4c
Randy Vianda Putra <modulloe@gmail.com>
2019-12-07 11:13:23 +0700

    fix: cors reverted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('./db/mongoose')
-
+const cors = require('cors')
 const app = express()
 const port = 3000
 
@@ -8,6 +8,7 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 app.use(express.json())
+app.use(cors())
 app.use(userRouter)
 app.use(taskRouter)
 
